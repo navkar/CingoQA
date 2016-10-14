b9257cd8c53fa01c919d4ca1a347a589e788887f
naveenkaram <navkar@outlook.com>
2016-10-13 19:39:34 -0500

    updated gems

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,13 +3,13 @@ class AnswersController < ApplicationController
   def create
     question = Question.find(params[:answer][:question_id])
     answer = question.answers.create(answer_params)
-  begin
-    # Active job with Sucker punch
-    MainMailer.notify_question_author(answer).deliver_later
-  ensure
+    begin
+      # Active job with Sucker punch
+      MainMailer.notify_question_author(answer).deliver_later
+    rescue
+    end
     session[:current_user_email] = answer_params[:email]
     redirect_to question
-  end
   end
 
   private
